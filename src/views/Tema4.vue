846e3b5b452aabcb7c4e42f85a65e698f25ae49c
Andrea Botello <andrea.botellor@misena.edu.co>
2024-10-03 15:12:18 -0500

    FinalNath

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,9 +5,247 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Aja
+      h1 El conflicto
     
+    .fullview-2.p-4
+      .row.justify-content-center.mb-0
+        .col-sm-12.col-lg-11.position-relative
+          .crd.crd--avatarHorizontal.crd--first.py-0
+            .row.align-items-center.py-avatarHorizontal 
+              .col
+                .row.justify-content-center.align-items-center.mb-3
+                p.mb-0 Los conflictos se presentan en organizaciones, familias, grupos como resultado de una mala comunicación. Se presenta en una relación comercial cuando hay un desacuerdo entre los intereses del cliente y los requisitos definidos. Cuando se crea una brecha entre cliente y organización, sin alcanzar acuerdos.
+                .row.justify-content-center.align-items-center.mb-3 
+                  .col-sm-12.col-lg-2.d-none.d-lg-block
+                    img(src='@/assets/curso/temas/t4/img2.svg', alt='')
+                  .col-sm-12.col-lg-10
+                    p.mb-0 El conflicto da como resultado la insatisfacción del cliente, afectando la imagen de la entidad, situación que puede ser perjudicial en el posicionamiento dentro del ámbito comercial, por eso, es importante entrar a analizar la causa por la cual se originó el conflicto, los intereses que estaban en juego y qué consecuencias trae para la empresa a corto y mediano plazo, con el fin de poder plantear acciones de mejora encaminadas a generar soluciones definitivas frente a la situación detectada.
+              .col-auto.d-none.d-lg-block
+                  img(src="@/assets/curso/temas/t4/img1.png", alt="alt").img100
     
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-4
+        img(src='@/assets/curso/temas/t4/img3.svg', alt='')
+      .col-lg-8
+        p.mb-2 La situación de conflicto puede convertirse en una oportunidad para obtener resultados positivos, si se entra a revisar los hechos, se reflexiona sobre ellos y se asumen acciones de mejoramiento.
+        p Se presentan ocasiones en que los conflictos permanecen, pues se tiene la concepción de que en todo conflicto debe existir un ganador, no se privilegia el consenso. Por tanto, ante el conflicto los actores pueden tomar una posición de dominación (ganador - perdedor) o por el contrario de asociación (ganador - ganador) llegando a acuerdo que beneficien a ambas partes.
+    
+    p.mb-4 Para enfrentar un conflicto es necesario: 
+
+    .row.mb-4
+      .col-lg-3
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/t4/tarjeta1.svg', alt='')
+          .tarjeta.bg2
+            .p-4
+              p #[b Aprender a ser objetivos y analizar.]
+      .col-lg-3
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/t4/tarjeta2.svg', alt='')
+          .tarjeta.bg2
+            .p-4
+              p #[b Aprender a escuchar y argumentar.]
+      .col-lg-3
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/t4/tarjeta3.svg', alt='')
+          .tarjeta.bg2
+            .p-4
+              p #[b Tomar decisiones.]
+      .col-lg-3
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/t4/tarjeta4.svg', alt='')
+          .tarjeta.bg2
+            .p-4
+              p #[b Aprender a recapacitar.]
+    
+    .bgimg4.mb-3
+      h4.pt-1 Tipos de conflictos 
+    
+    p.mb-4 Aunque es dificultoso y arriesgado enumerar de forma precisa los conflictos se han jerarquizado las siguientes categorías que permiten orientar y entender asertivamente los conflictos. 
+    p.mb-4 Los conflictos pueden ser: 
+
+    .row.mb-4
+      .col-lg-6.bg1
+        .row.mb-2
+          .col-lg-2
+            img(src='@/assets/curso/temas/t4/img4.svg', alt='')
+          .col-lg-10
+            h4 Interpersonal
+        .row
+          p Se da cuando no se halla puntos de acuerdo con otras personas, ya sea en la familia, entre amigos, entre sujetos que no se conocen, entre colaboradores de una empresa y son producto de conflicto de intereses, discriminación, intolerancia, falta de información y dominación. 
+      .col-lg-6.bg5
+        .row.mb-2
+          .col-lg-2
+            img(src='@/assets/curso/temas/t4/img5.svg', alt='')
+          .col-lg-10
+            h4 Intrapersonal
+        .row
+          p Se produce cuando un individuo no se encuentra satisfecho consigo mismo, debido a pensamientos distorsionados y/o a creencias irracionales. Para superar estos conflictos se requiere del acompañamiento de un profesional de la psicología.  
+    
+    .bgimg4.mb-3
+      h4.pt-1 Elementos del conflicto 
+    
+    p.mb-4 Para que se produzca un conflicto deben estar presentes los siguientes elementos: 
+
+    .row.mb-5
+      .col-lg-4
+        .row
+          .col-lg-2
+            img(src='@/assets/curso/temas/t4/img6.svg', alt='')
+        p #[b Persona o personas:] con principios, valores, percepciones, emociones, sentimientos, intereses, posiciones y necesidades distintas que dan cuerpo al conflicto. 
+      .col-lg-4
+        .row
+          .col-lg-2
+            img(src='@/assets/curso/temas/t4/img6.svg', alt='')
+        p #[b Problemas:] dificultad a superar, queja o molestia. 
+      .col-lg-4
+        .row
+          .col-lg-2
+            img(src='@/assets/curso/temas/t4/img6.svg', alt='')
+        p #[b Proceso:] intervención, pasos encaminados a solucionar el conflicto.  
+    
+    .titulo-sexto.color-primario
+      h5 Fuentes del conflicto organizacional 
+    
+    .row.mb-5
+      .col-lg-7
+        p.mb-3 Una organización es una confluencia de pensamientos, habilidades, sentimientos, individualidades, creencias y vivencias, que impregnan las interrelaciones entre todos los participantes de una organización, por lo tanto es un escenario social donde existen fuentes inagotables de conflicto como: 
+        ol.lista-ol--cuadro.lista-ol--separador
+          li 
+            .lista-ol--cuadro__vineta
+              span 1
+            | Objetivos inalcanzables.
+          li 
+            .lista-ol--cuadro__vineta
+              span 2
+            | Desacuerdos, por generar producto de la variedad cultural que se presentan entre los colaboradores.
+          li 
+            .lista-ol--cuadro__vineta
+              span 3
+            | Incapacidad de trabajar en equipo. 
+          li 
+            .lista-ol--cuadro__vineta
+              span 4
+            | Recursos insuficientes, que dificultan el desarrollo de la labor asignada.
+          li 
+            .lista-ol--cuadro__vineta
+              span 5
+            | Normas confusas.
+          li 
+            .lista-ol--cuadro__vineta
+              span 6
+            | Dificultades en la comunicación.  
+      .col-lg-5
+        img(src='@/assets/curso/temas/t4/img7.png', alt='')
+    
+    .titulo-sexto.color-primario
+      h5 Variables del conflicto
+
+    p.mb-4 Para intervenir un conflicto o medir el grado de afectación que éste genera dentro de un contexto se deben analizar las siguientes variables: 
+
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-3.d-none.d-lg-block
+        img(src='@/assets/curso/temas/t4/img8.png', alt='')
+      .col-sm-12.col-lg-9
+        AcordionA.mb-0(tipo="a" clase-tarjeta="tarjeta tarjeta--orange")
+          .row(titulo="Resistencia al cambio")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Es el grado de rechazo o aceptación de un sujeto o un colectivo frente a nuevas orientaciones, procesos o procedimientos.
+          .row(titulo="Clima organizacional")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Percepción emocional de los colaboradores de una organización con relación a las interrelaciones que se generan dentro de ella. 
+          .row(titulo="Estilo de dirección")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Tipo de liderazgo ejercido por los directivos de una organización.
+          .row(titulo="Características personales")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Aspectos propios de un sujeto que lo identifica de los demás y que evidencia un sello único en el actuar. 
+          .row(titulo="Normatividad")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Conjunto de disposiciones que reglamentan una organización. 
+          .row(titulo="Técnicas de motivación")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Métodos empleados por una organización para mantener el interés y compromiso de sus colaboradores para el desarrollo de sus actividades.  
+    
+    .titulo-sexto.color-primario
+      h5 Manejo y resolución
+    
+    p.mb-4 Como lo dice Viveros (2003): 
+    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8   
+        .bloque-texto-b.color-primario.p-4
+          .bloque-texto-b__texto
+            i.fas.fa-quote-left
+            h2.mb-0 Los conflictos siempre existen y existirán, por lo cual nuestra obligación como dirigentes es aprender a vivir con ellos tratando que no se manifiesten y administrándolos eficientemente cuando ocurren.
+            i.fas.fa-quote-right
+    
+    p.mb-4 Las actitudes que pueden prevenir un conflicto y contribuir con el desarrollo organizacional son: 
+    
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+      .tarjeta.color-acento-contenido.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/t4/slyder1.svg', alt='')
+        h5.text-center Paciencia
+      .tarjeta.color-acento-contenido.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/t4/slyder2.svg', alt='')
+        h5.text-center Agradecimiento
+      .tarjeta.color-acento-contenido.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/t4/slyder3.svg', alt='')
+        h5.text-center Comunicación
+      .tarjeta.color-acento-contenido.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/t4/slyder4.svg', alt='')
+        h5.text-center Capacitación
+      .tarjeta.color-acento-contenido.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/t4/slyder5.svg', alt='')
+        h5.text-center Fidelidad
+      .tarjeta.color-acento-contenido.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/t4/slyder6.svg', alt='')
+        h5.text-center Cooperación
+      .tarjeta.color-acento-contenido.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/t4/slyder7.svg', alt='')
+        h5.text-center Retroalimentación
+
+    p.mb-4 Ante un conflicto se pueden asumir las siguientes posiciones: 
+
+    .row.mb-5
+      .col-md-6.col-xl.mb-4.mb-xl-0
+        .tarjeta-numerada.color-primario.p-5
+          .tarjeta-numerada__numero
+            .h2 1 
+          p.text-center #[b Solucionar el conflicto o negociar. ]
+      .col-md-6.col-xl.mb-4.mb-xl-0
+        .tarjeta-numerada.color-acento-contenido.p-5
+          .tarjeta-numerada__numero
+            .h2 2
+          p.text-center #[b Aceptar o ceder ante la situación conflictiva. ]
+      .col-md-6.col-xl.mb-4.mb-xl-0
+        .tarjeta-numerada.color-acento-botones.p-5
+          .tarjeta-numerada__numero
+            .h2 3
+          p.text-center #[b Imponer.]
+      .col-md-6.col-xl.mb-4.mb-xl-0
+        .tarjeta-numerada.color-secundario.p-5
+          .tarjeta-numerada__numero
+            .h2 4 
+          p.text-center #[b Evitar, retirarse.]
 
 </template>
 
--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -5,10 +5,117 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 5
-      h1 Aja
+      h1 Medición de la satisfacción del cliente
     
-    
+    //banner
 
+    .row.mb-5
+      .col-lg-7
+        p.mb-3 Una lista básica de ellos podría ser:
+        ol.lista-ol--cuadro.lista-ol--separador
+          li 
+            .lista-ol--cuadro__vineta
+              span 1
+            | Oferta del servicio o producto.
+          li 
+            .lista-ol--cuadro__vineta
+              span 2
+            | Admisión y entrega de pedidos.
+          li 
+            .lista-ol--cuadro__vineta
+              span 3
+            | Proyección de nuevos clientes.
+          li 
+            .lista-ol--cuadro__vineta
+              span 4
+            | Respuesta a sugerencias, quejas y reclamos.
+          li 
+            .lista-ol--cuadro__vineta
+              span 5
+            | Indagación sobre empresas con igual idea de negocio.
+          li 
+            .lista-ol--cuadro__vineta
+              span 6
+            | Planear estrategias de venta. 
+          li 
+            .lista-ol--cuadro__vineta
+              span 7
+            | Realización de un plan de mercadeo (marketing).
+          li 
+            .lista-ol--cuadro__vineta
+              span 8
+            | Creación o mejoramiento de productos o servicios. 
+          li 
+            .lista-ol--cuadro__vineta
+              span 9
+            | Habilidad para definir ofertas y promociones. 
+      .col-lg-5
+        img(src='@/assets/curso/temas/t5/img1.svg', alt='')
+    
+    p.mb-3 Tener claridad de las actividades y procedimientos que se lleva a cabo en la empresa permite la rápida identificación de dificultades, con el fin de poderla convertir en una posibilidad de mejoramiento; el reconocimiento por parte de todos los colaboradores facilita brindar una información oportuna y veraz a los clientes.
+    p.mb-4 Si se desea medir la satisfacción del cliente, se debe estar atento y dar respuesta oportuna a: 
+    
+    TabsC.color-acento-contenido.mb-5
+      .py-3.py-md-4(titulo="Petición")
+        .row.justify-content-center.align-items-center.bg3
+          .col-md-6.mb-4.mb-md-0
+            img(src='@/assets/curso/temas/t5/tabs1.png', alt='')
+          .col-md-6
+            h4 Petición
+            p La solicitud que hace un cliente con relación al servicio que le prestan.
+
+      .py-3.py-md-4(titulo="Queja")
+        .row.justify-content-center.align-items-center.bg3
+          .col-md-6.mb-4.mb-md-0
+            img(src='@/assets/curso/temas/t5/tabs2.png', alt='')
+          .col-md-6
+            h4 Queja
+            p La manifestación de inconformidad o insatisfacción, relacionado con el servicio o la atención que ha recibido. 
+
+      .py-3.py-md-4(titulo="Reclamo")
+        .row.justify-content-center.align-items-center.bg3
+          .col-md-6.mb-4.mb-md-0
+            img(src='@/assets/curso/temas/t5/tabs3.png', alt='')
+          .col-md-6
+            h4 Reclamo
+            p La expresión de total rechazo al servicio o la atención recibida; el cliente considera que se llegó al desconocimiento de sus derechos.  
+    
+    p.mb-3 Estos tres elementos, se transforman no en una amenaza, por el contrario, se convierten en una oportunidad de mejorar, ya que permite llamar la atención sobre los aspectos en que están fallando las organizaciones. Es así como se debe asumir una actitud serena, atenta y de calma, que facilite la reflexión y retroalimentación con el fin de establecer acciones encaminadas al mejoramiento. 
+
+    .bgimg5.mb-3
+      h4.pt-1 Servicio de información y atención al usuario (cliente o ciudadano)
+    
+    .row.mb-4
+      .col-lg-9
+        p.mb-2 Este servicio constituye un canal de comunicación e interacción directa con los usuarios, se encarga de establecer estrategias que midan la satisfacción de los clientes. Una de estas tácticas puede ser la atención directa, encuestas de satisfacción o la creación del buzón de sugerencias, que al igual que la encuesta, deben ser revisados constantemente con el fin de dar respuesta oportuna a la inconformidad o malestar.
+        p.mb-4 El análisis, desde una mirada crítica y la reflexión sobre las acciones que generaron la insatisfacción, permite realizar acciones: 
+    
+    .row.mb-5
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar
+          img(src='@/assets/curso/temas/t5/tarjeta1.svg', alt='')
+          .tarjeta.bg6
+            h2.text-center Correctivas
+            p.text-center Encaminadas a que los hechos que provocaron la inconformidad no se repitan.  
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar
+          img(src='@/assets/curso/temas/t5/tarjeta2.svg', alt='')
+          .tarjeta.bg6
+            h2.text-center Preventivas
+            p.text-center Diseñadas para evitar que sucedan situaciones que puedan afectar la labor de la empresa.
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar
+          img(src='@/assets/curso/temas/t5/tarjeta3.svg', alt='')
+          .tarjeta.bg6
+            h2.text-center Mejora continua
+            p.text-center Gestiones que se efectúan a diario como parte de la búsqueda de la excelencia constante.
+    
+    p.mb-4 Gracias a estas estrategias las empresas pueden alcanzar un posicionamiento en el mercado, incursionar en nuevos negocios e inclusive innovar en el servicio o producto: 
+    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6.bg7
+        p.text-center #[b El cliente es el mejor aliado para las organizaciones que desean prosperar. ]
+        
 </template>
 
 <script>
