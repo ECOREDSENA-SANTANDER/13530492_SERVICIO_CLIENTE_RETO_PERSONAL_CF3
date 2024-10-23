bc57b4f78240d9f7f108d97d3b49bed29c98e090
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2024-10-23 18:16:29 -0500

    Act ajustes

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -14,7 +14,7 @@
     figure
       .video
         <iframe width="560" height="315" src="https://www.youtube.com/embed/CJgWyksgqa8?si=VAKlUo3qHSYbebXN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      figcaption Video: Leyenda del video
+      figcaption Video. Interacción con el cliente
     
 </template>
 
